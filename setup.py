c27a3a38f8992541151ceefc20f7db59626b47aa
Christopher Grebs <cg@webshox.org>
2014-02-28 18:27:43 +0100

    Add 2.6 classifier

--- setup.py
+++ setup.py
@@ -87,6 +87,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: Implementation :: CPython',
