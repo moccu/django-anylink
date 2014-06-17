f642d703ca7e614b6af0ae72c51a1002fa444798
Christopher Grebs <cg@webshox.org>
2014-06-17 14:54:47 +0200

    Don't use sphinx_rtd_theme on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,16 @@ exclude_patterns = ['build']
 
 pygments_style = 'sphinx'
 
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# on_rtd is whether we are on readthedocs.org,
+# this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:
+    # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 # html_theme_options = {}
 # html_static_path = ['_static']
 htmlhelp_basename = 'django-anylinkdoc'
