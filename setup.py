23af41fec2f7ab15ba18fc3c0528e81590438d04
Christopher Grebs <cg@webshox.org>
2015-03-30 18:05:17 +0200

    Remove python 3.3 from classifiers.

--- setup.py
+++ setup.py
@@ -86,7 +86,6 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: Implementation :: CPython',
