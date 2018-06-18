4ac176c0f1e6400f9472d54d5542df741f2768c3
Maik Hoepfel <m@maikhoepfel.de>
2018-06-18 16:42:47 +0200

    Python's unordered dictionaries make life difficult
    I can't find a reliable way to test the whole HTML output - it changes
    between Python versions, or, more likely: between each run as
    dictionaries don't have a given order. This test is more robust.

--- testing/pytests/test_widgets.py
+++ testing/pytests/test_widgets.py
@@ -16,16 +16,9 @@ class TestAnyLinkAddOrChangeWidget:
 
     def test_form_output_empty(self):
         out = DummyForm().as_p()
-        assert out == (
-            '<p><label for="id_link">Link:</label> <input type="hidden" name="l'
-            'ink" required id="id_link" /><strong id="name_id_link"></strong>&nbsp;<a hr'
-            'ef="/admin/anylink/anylink/?_to_field=id" class="anylink-button sh'
-            'ow-popup" id="lookup_id_link" onclick="return window.AnyLinkAddOrCh'
-            'angeWidget.show(this, true);" data-add="Add link" data-change="Ch'
-            'ange link">Add link</a>&nbsp;<img src="/static/admin/img/icon-deletelink.svg" id="'
-            'delete_id_link" onclick="return window.AnyLinkAddOrChangeWidget.de'
-            'lete(this);" style="cursor:pointer;display:none" /></p>'
-        )
+        assert '/admin/anylink/anylink/' in out
+        assert 'class="anylink-button show-popup"' in out
+        assert 'onclick="return window.AnyLinkAddOrChangeWidget' in out
 
     def test_form_output_with_instance(self):
         link = AnyLink.objects.create(
@@ -33,16 +26,10 @@ class TestAnyLinkAddOrChangeWidget:
         obj = DummyModel.objects.create(link=link)
 
         form = DummyForm(instance=obj)
-        assert form.as_p() == (
-            '<p><label for="id_link">Link:</label> <input type="hidden" name="li'
-            'nk" value="1" required id="id_link" /><strong id="name_id_link">/fake/</'
-            'strong>&nbsp;<a href="/admin/anylink/anylink/?_to_field=id" class="a'
-            'nylink-button show-popup" id="lookup_id_link" onclick="return wind'
-            'ow.AnyLinkAddOrChangeWidget.show(this, true);" data-add="Add link"'
-            ' data-change="Change link">Change link</a>&nbsp;<img src="/static/'
-            'admin/img/icon-deletelink.svg" id="delete_id_link" onclick="return window.AnyLinkA'
-            'ddOrChangeWidget.delete(this);" style="cursor:pointer" /></p>'
-        )
+        out = form.as_p()
+        assert '/admin/anylink/anylink/' in out
+        assert 'class="anylink-button show-popup"' in out
+        assert 'onclick="return window.AnyLinkAddOrChangeWidget' in out
 
     def test_form_media(self):
         out = str(DummyForm().media)
