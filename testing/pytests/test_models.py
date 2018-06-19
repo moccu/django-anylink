417d5ca0a3bd675e313434fcb51134df023bdc62
Stephan Jaekel <steph@rdev.info>
2018-06-19 14:29:18 +0200

    Refactor tests to prevent Django from complaining about Re-registered models.

--- testing/pytests/test_models.py
+++ testing/pytests/test_models.py
@@ -24,24 +24,26 @@ class DummyExtension(BaseLink):
 
 
 class TestAnyLinkMetaclass:
-    def setup(self):
-        class AnyLinkTest(AnyLink):
-            class Meta:
-                app_label = 'AnyLinkTest'
-
-        self.anylink_test_class = AnyLinkTest
 
     def test_extension_registration(self, settings):
+        class AnyLinkTest(AnyLink):
+            class Meta:
+                app_label = 'anylinktest'
+
         settings.ANYLINK_EXTENSIONS = (
             'anylink.extensions.ExternalLink',
         )
 
-        do_anylink_extension_setup(self.anylink_test_class)
+        do_anylink_extension_setup(AnyLinkTest)
 
-        assert len(self.anylink_test_class.extensions) == 1
-        assert hasattr(self.anylink_test_class, 'get_link_type_display')
+        assert len(AnyLinkTest.extensions) == 1
+        assert hasattr(AnyLinkTest, 'get_link_type_display')
 
     def test_choices_sorting(self, settings):
+        class AnyLinkTestSorting(AnyLink):
+            class Meta:
+                app_label = 'anylinktest'
+
         extensions = (
             ('anylink.extensions.ModelLink', {'model': 'testproject.LinkableObject'}),
             'testing.pytests.test_models.DummyExtension',
@@ -50,32 +52,42 @@ class TestAnyLinkMetaclass:
 
         settings.ANYLINK_EXTENSIONS = extensions
 
-        do_anylink_extension_setup(self.anylink_test_class)
+        do_anylink_extension_setup(AnyLinkTestSorting)
 
         choices_keys = list(
-            OrderedDict(self.anylink_test_class._meta.get_field('link_type').choices).keys())
+            OrderedDict(AnyLinkTestSorting._meta.get_field('link_type').choices).keys())
 
         assert choices_keys == ['dummy', 'external_url', 'linkableobject']
 
     def test_extension_registration_twice(self, settings):
+        class AnyLinkTestTwice(AnyLink):
+            class Meta:
+                app_label = 'anylinktest'
+
         settings.ANYLINK_EXTENSIONS = (
             'anylink.extensions.ExternalLink',
             'anylink.extensions.ExternalLink',
         )
 
         with pytest.raises(ImproperlyConfigured):
-            do_anylink_extension_setup(self.anylink_test_class)
+            do_anylink_extension_setup(AnyLinkTestTwice)
 
     def test_extension_registration_with_options(self, settings):
+        class AnyLinkTestWithOptions(AnyLink):
+            class Meta:
+                app_label = 'anylinktest'
+
         settings.ANYLINK_EXTENSIONS = (
             ('anylink.extensions.ExternalLink', {'test': 'foo'}),
         )
 
-        do_anylink_extension_setup(self.anylink_test_class)
+        do_anylink_extension_setup(AnyLinkTestWithOptions)
 
-        assert len(self.anylink_test_class.extensions) == 1
-        assert self.anylink_test_class.extensions['external_url'].kwargs == {'test': 'foo'}
-        assert hasattr(self.anylink_test_class, 'get_link_type_display')
+        assert len(AnyLinkTestWithOptions.extensions) == 1
+        assert AnyLinkTestWithOptions.extensions['external_url'].kwargs == {
+            'test': 'foo'
+        }
+        assert hasattr(AnyLinkTestWithOptions, 'get_link_type_display')
 
 
 class TestAnyLink:
